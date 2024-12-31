eea2857b83abd87fc0e3cedbbb3a9c5652ebc79e
KAZ <kazdoescode@gmail.com>
2024-12-31 20:54:56 +0100

    Conditions twist with ticket input with age

--- website/conditions/index.js
+++ website/conditions/index.js
@@ -1,5 +1,32 @@
-document.getElementById('myH1').textContent = "This is a test de ma part"
+// IF STATEMENTS = if a condition is true, execute some code
+//                                   if not, do something else
 
-function() {
+const myText = document.getElementById("input");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function(){
+
+    age = myText.value;
+    age = Number(age);
     
+    if(age >= 65){
+        resultElement.textContent = `Senior ticket 15$`;
+    }
+    else if(age >= 0 && age <= 5){
+        resultElement.textContent = `Free entry for kids under 6.`
+    }
+    else if(age >= 6 && age <= 17){
+        resultElement.textContent = `Child ticket: $10.`
+    }
+    else if(age >= 18 && age <= 64){
+        resultElement.textContent = `Adult ticket: $20.`
+    }
+
+    else{
+        resultElement.textContent = `Your age can't be negative`;
+    }
 }
+
+//Use of Number() to Convert Input: Ensuring the input is treated as a number is good practice.
