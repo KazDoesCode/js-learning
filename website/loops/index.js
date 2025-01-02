909e6d573d561779f64fdb3f11dc096b15d9b6b6
KAZ <kazdoescode@gmail.com>
2025-01-02 21:47:51 +0100

    GUESSING GAME MY VERSION

--- website/loops/index.js
+++ website/loops/index.js
@@ -1,3 +1,38 @@
+// Number guessing tuto with While loop
+
+let min = 1;
+let max = 100;
+
+
+let answer = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log("The answer was !",answer);
+
+let attempts = 0;
+let guess;
+let run = true;
+ 
+while(run) {                // The condition MUST be TRUE in order to be in the while condition 
+
+    guess = window.prompt("Enter your guess");
+    guess = Number(guess);                // Very usefull to convert whatever I have into a number !
+    console.log(typeof guess, guess);     // Check the type of my variable
+    if(guess === answer) {
+        console.log("THAT's right !!!!");
+        run = false;  // HERE we got the right answer and this is where we wanna exit the loop
+    }
+
+    else if (guess != answer) {
+        console.log("WRONG ! Repeat");   
+
+    }
+
+    
+}
+
+
+
+/*
 const p1 = document.getElementById("p1");
 const input1 = document.getElementById("input1");
 const but1 = document.getElementById("but1");
@@ -54,7 +89,7 @@ do{
 
 // for loop while using continue and break
 
-
+/*
 for(let i = 1; i<= 10; i++) {
     if(i==7){
         continue; //here continue will skip number 13 and will only print 1-2-3-4-5-6-8-9-10
@@ -66,4 +101,4 @@ for(let i = 1; i<= 10; i++) {
 }
 
 console.log('GOOOOOO');
-
+*/
