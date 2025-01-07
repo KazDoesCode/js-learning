de448877cc9541729b776a38c8d64429fce6f46a
KAZ <kazdoescode@gmail.com>
2025-01-07 12:38:52 +0100

    Done version 1 HTML CSS JS

--- Portfolio/Dice Roller Program/index.js
+++ Portfolio/Dice Roller Program/index.js
@@ -1,3 +1,24 @@
 const button = document.getElementById("DiceRoller")
 const result = document.getElementById("resultElement")
-const dice_input = document.getElementById("dice-input")
+
+
+function rollDice() {
+    
+    const dice_input = document.getElementById("dice-input").value
+    const diceResults = document.getElementById("DiceResultsDiv")
+    const diceImages = document.getElementById("DiceImagesDiv")
+    const values=[]   // Our Rolled Dices in a row values (1-6)
+    const images=[]   // Images of our Rolled dices in a row
+
+    for (let index = 0; index < dice_input; index++) {
+        const value = Math.floor(Math.random() * 6) + 1
+        values.push(value)
+        images.push(`<img src="images/${value}.png">`)
+    }
+
+    result.textContent = values
+    diceImages.innerHTML = images
+
+
+    
+}
