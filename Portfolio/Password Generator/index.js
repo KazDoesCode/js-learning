929feab77c33dbcc053f8ea3b266a01c3fe3d960
KAZ <kazdoescode@gmail.com>
2025-01-10 22:35:43 +0100

    Password gen v2 with user interface

--- Portfolio/Password Generator/index.js
+++ Portfolio/Password Generator/index.js
@@ -19,7 +19,7 @@ function generatepassword(length, includeLowercase, includeUppercase, includeSym
     if (length <= 0 ) { return "Password must be atleast one character !"   }
     
     if (allowedchars.length === 0 ) {
-        return "You need atleast one activated"
+        return console.log("You need atleast one activated")
     }
 
     for(let i = 0 ; i < length; i++){
@@ -30,12 +30,21 @@ function generatepassword(length, includeLowercase, includeUppercase, includeSym
 
     return password
 }
-const passlen = 12;
-const includeLowercase = true;
-const includeNumbers = true;
-const includeUppercase = true;
-const includeSymbols = true;
 
-const password = generatepassword(passlen, includeLowercase, includeUppercase, includeSymbols, includeNumbers)
+document.getElementById("PassGen").addEventListener("click", () => {
 
-console.log(`Password generated is ${password}`)
+        const passlen = document.getElementById("len-input").value;
+
+        const includeLowercase = document.getElementById("Lowercase").checked;
+        const includeNumbers = document.getElementById("numbers").checked;
+        const includeUppercase = document.getElementById("Uppercase").checked;
+        const includeSymbols = document.getElementById("symbols").checked;
+
+
+        const password = generatepassword(passlen, includeLowercase, includeUppercase, includeSymbols, includeNumbers)
+
+        console.log(`Password generated is ${password}`)
+
+        document.getElementById("resultElement").textContent = password
+
+})
